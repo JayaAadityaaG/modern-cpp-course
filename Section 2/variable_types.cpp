dfd9a088790cd2cb4f8255721a5df13ac3f5a87c
Jaya Aadityaa <jayaaadityaa98@gmail.com>
2023-01-08 12:47:37 -0500

    variable types

---
+++ Section 2/variable_types.cpp
@@ -0,0 +1,17 @@
+#include<iostream>>
+using namespace std;
+int main(){
+    //Scalar types
+    int i=0;
+    char ch='a';
+    bool flag = false; //true or false 
+    // Zero value for boolean is false and everything else is true
+
+    float f = 1.283f; // Float suffix is necessary 
+    double d = 1.2424324;
+
+    //Vector types 
+    int arr[5]; //Integer array 
+    int arr[5] = {1,2,3,4,5}; 
+
+}
